e3676ee432dcfa206406c4e8571f7759036c7871
JenMelghem <alemelghem01@gmail.com>
2023-02-19 17:51:39 -0600

    Enteros

---
+++ src/main/java/Valor/Enteros.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Enteros
+
+{ // Abre clase Deitel_4_33
+
+public void Imprime()
+{ // Abre metodo Imprime
+System.out.println("\nEste programa imprime todos los multiplos de 2, es infinito!\n");
+int i = 1;
+
+while( i > 0)
+{ // Abre while
+System.out.printf("\n%d", 2*i++);
+} // Cierra while
+
+} // Cierra imprime
+
+} // Cierra clase Ejercicio4_33
