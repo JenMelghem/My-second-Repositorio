9458183e6691274b35471fb9ff82b90dd7dbd7d1
JenMelghem <alemelghem01@gmail.com>
2023-02-19 17:16:14 -0600

    Hola

--- src/main/java/Ejercicios.java
+++ src/main/java/Ejercicios.java
@@ -1,12 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Melghem
- */
-public class Ejercicios {
-    
-}
+import java.util.Scanner;
+public class Salarios
+
+{ // Abre clase Salarios
+
+Scanner entrada = new Scanner(System.in);
+
+public void Recibe()
+{ // Abre metodo Recibe
+int numeroEmpleado = 1;
+int horas = 0;
+double salario = 0;
+double total = 0;
+
+while ( 3 >= numeroEmpleado)
+{ // Abre while
+System.out.printf("\nPor favor introduzca el numero de horas que trabajo el empleado %d: ", numeroEmpleado);
+
+horas = entrada.nextInt();
+
+System.out.printf("\nIntroduzca por favor el salario por hora del empleado %d: ", numeroEmpleado);
+salario = entrada.nextDouble();
+
+if ( 40 >= horas)
+total = horas*salario;
+else
+total = 40*salario + (horas - 40)*(salario + salario/2);
+
+System.out.printf("\nEl salario del empleado numero %d es %.2f\n", numeroEmpleado++, total);
+} // Cierra while
+
+
+} // Cierra metodo Recibe
+
+} // Cierra clase Salarios
